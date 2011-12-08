7abf92f1a05de1338b91d836f0999d7b8324455e
jrconlin <jconlin+git@mozilla.com>
2011-12-07 16:30:00 -0800

    Added OAuth code and tests

--- bipostaldash/auth/oauth.py
+++ bipostaldash/auth/oauth.py
@@ -1,5 +1,10 @@
+import base64
+import hashlib
+import hmac
 import urllib2
 import re
+from services import logging
+
 
 class NonceRing(object):
 
@@ -22,91 +27,98 @@ class NonceRing(object):
 
 nonces = NonceRing()
 
-def _oEsc(string):
-    if not string:
-        return ''
-    string = urllib2.quote(string)
-    return string.replace('/', '%2F').replace('+', '%20')\
-            .replace('!', '%21').replace('*', '%2A')\
-            .replace('\\', '%27').replace('(', '%28').\
-            replace(')', '%29')
+
+class OAuthException(Exception):
+    def __init__(self, value):
+        self.msg = value
+
+    def __str__(self):
+        return repr(self.msg)
 
 
-def normalizedParams(params):
-    if not params:
-        return ''
-    elements = []
-    pkeys = params.keys()
-    pkeys.sort()
-    for pname in pkeys:
-        if '_secret' in pname:
-            continue
-        pvalue = params.get(pname)
-        if type(pvalue) == type([]):
-            pvalue.sort()
-            for value in pvalue:
-                elements.append("%s=%s" % (_oEsc(pname),
-                                           _oEsc(value)))
-            next
-        else:
-            elements.append("%s=%s" % (_oEsc(pname),
-                                       _oEsc(pvalue)))
-    return '&'.join(elements)
+class OAuth(object):
+    """ Absolutely minimal OAuth function to validate args and return
+        an email. """
+
+    def _oEsc(self, string):
+        if not string:
+            return ''
+        string = urllib2.quote(str(string))
+        return string.replace('/', '%2F').replace('+', '%20')\
+                .replace('!', '%21').replace('*', '%2A')\
+                .replace('\\', '%27').replace('(', '%28').\
+                replace(')', '%29')
 
 
-def sign(method, path, tokens, params, keys):
-    sbs = '&'.join((method.upper(),
-                    path.upper(),
-                    normalizedParams(params)))
-    
+    def normalizedParams(self, params):
+        if not params:
+            return ''
+        elements = []
+        pkeys = params.keys()
+        pkeys.sort()
+        for pname in pkeys:
+            if '_secret' in pname or pname == 'oauth_signature':
+                continue
+            pvalue = params.get(pname)
+            if type(pvalue) == type([]):
+                pvalue.sort()
+                for value in pvalue:
+                    elements.append("%s=%s" % (self._oEsc(pname),
+                                               self._oEsc(value)))
+                next
+            else:
+                elements.append("%s=%s" % (self._oEsc(pname),
+                                           self._oEsc(pvalue)))
+        return '&'.join(elements)
 
 
+    def get_user_id(self, request):
+        reg = request.registry
+        params = request.params
 
-def authenticated_userid(request):
-    reg = request.registry
-    params = request.params
+        # Do we have keys?
+        try:
+            consumer_key = reg['config']['oauth.consumer_key']
+            consumer_secret = self._oEsc(reg['config']['oauth.consumer_secret'])
+        except KeyError, e:
+            raise OAuthException('Config missing keys: "%s"' % str(e))
+        # Is this an OAuth request?
+        if 'Authorization' in request.headers:
+            auth_header = request.headers.get("Authorization")
+            if not auth_header.startswith('OAuth '):
+                raise Exception('Request is not signed using OAuth')
+            grp = re.compile('(\w+)="([^"]+)"')
+            for match in grp.finditer(auth_header):
+                group = match.groups()
+                params[group[0]] = group[1]
+        # Do we have the required OAuth values?
+        required_keys = ['oauth_nonce',
+                         'oauth_timestamp',
+                         'oauth_consumer_key',
+                         'oauth_signature',
+                         'oauth_signature_method',
+                         'oauth_version']
+        for key in required_keys:
+            if key not in params:
+                raise OAuthException('Request missing required OAuth values')
+        your_sig = urllib2.unquote(params.get('oauth_signature'))
+        # Have we seen this nonce for this consumer_key before?
+        if (nonces.contains(params['oauth_nonce'])):
+            raise OAuthException('Repeated oauth_nonce')
+        nonces.add(params['oauth_nonce'])
+        path = request.path
+        method = request.method
+        # Build the comparison SBS
+        sbs = '&'.join(((self._oEsc(method.upper())), 
+                self._oEsc(path), 
+                self._oEsc(self.normalizedParams(params))))
+        # and generate the sig
+        test_sig = base64.b64encode(hmac.new("%s&" % consumer_secret,
+            sbs, hashlib.sha1).digest())
+        # Compare the comparison sig with the recv'd signature
+        if (params.get('oauth_consumer_key') == consumer_key 
+                and test_sig == your_sig):
+            return params.get('email')
+        logging.error("invalid signature")
+        return None
 
-    # Do we have keys?
-    try:
-        public_key = reg['config']['oauth.public_key']
-        private_key = reg['config']['oauth.private_key']
-    except KeyError, e:
-        raise Exception('Config missing keys: "%s"' %
-                                   str(e))
-    # Is this an OAuth request?
-    if 'Authorization' in request.headers:
-        auth_header = request.headers.get("Authorization")
-        if not auth_header.startswith('OAuth '):
-            raise Exception('Request is not signed using OAuth')
-        oauth_token = {}
-        for match in re.finditer('(\w+)="(\w+)"'):
-            group = match.groups()
-            oauth_token[group[0]] = group[1]
-    else:
-        for param in params:
-            if param.startswith('oauth_'):
-                oauth_token[param] = params.get(param)
-                del(params[param])
-    # Do we have the required OAuth values?
-    required_keys = ['oauth_nonce',
-                     'oauth_timestamp',
-                     'oauth_consumer_key',
-                     'oauth_signature_method',
-                     'oauth_signature',
-                     'oauth_version']
-    for key in required_keys:
-        if key not in oauth_token:
-            raise Exception('Request missing required OAuth values')
-    # Have we seen this nonce for this public_key before?
-    if (nonces.contains(oauth_token['oauth_nonce'])):
-        raise Exception('Repeated oauth_nonce')
-    path = request.path
-    method = request.method
-    # Build the comparison SBS
-    signature = sign(method=method,
-                        path=path,
-                        tokens=oauth_token,
-                        params=params)
-    # and generate the sig
-
-    # Compare the comparison sig with the recv'd signature.
---
+++ bipostaldash/tests/test_oauth.py
@@ -0,0 +1,49 @@
+import unittest2
+import os
+
+from pyramid import testing
+from nose.tools import (eq_, assert_raises)
+from bipostaldash.auth.oauth import (OAuth, OAuthException)
+
+class OAuthTest(unittest2.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+        self.auth = OAuth()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_authenticate(self):
+        request = testing.DummyRequest()
+        request.params['email'] = 'foo@example.com'
+        request.path = '/test'
+        request.method = 'GET'
+        request.params = {'a': 1, 'email': 'test@example.com'}
+
+        request.registry['oauth_params'] = {
+                'oauth_consumer_key': '123',
+                'oauth_nonce': '1234',
+                'oauth_timestamp': 1323283311,
+                'oauth_signature': 'gjKO7mKe7pRZ%2BsPlVnSgkAend3g%3D',
+                'oauth_signature_method': 'HMAC-SHA1',
+                'oauth_version': '1.0'}
+        request.registry['config'] = {
+                'oauth.consumer_key': '123',
+                'oauth.consumer_secret': 'abc'}
+        authElem = []
+        for key in request.registry['oauth_params']:
+            authElem.append('%s="%s"' % (key, 
+                request.registry['oauth_params'][key]))
+        authLine = 'OAuth %s' % ', '.join(authElem)
+        request.headers['Authorization'] = authLine
+        r_email = self.auth.get_user_id(request)
+        eq_(r_email, request.params['email'])
+        # Replay check
+        assert_raises(OAuthException, 
+                self.auth.get_user_id,
+                request)
+        
+
+
+
+         
