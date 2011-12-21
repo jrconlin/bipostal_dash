6589a85c77519067876318fb3ebbfe8d5a615540
jrconlin <jconlin+git@mozilla.com>
2011-12-21 11:06:07 -0800

    Added remote browserid verify

--- bipostaldash/auth/browserid.py
+++ bipostaldash/auth/browserid.py
@@ -1,16 +1,18 @@
-from services import logger
+from StringIO import StringIO
 from base64 import urlsafe_b64decode
-import urllib2
+from services import logger
 import json
+# urllib doesn't know https. pycurl does. 
+import pycurl
+import urllib
 import urllib2
 
 
 class BrowserIDAuth(object):
 
     def __init__(self, 
-            auth_server='browserid.org', 
             **kw):
-        self._server = auth_server
+        pass
 
     def create_user(self, assertion=None, **kw):
         self._raw_assertion = assertion
@@ -23,18 +25,44 @@ class BrowserIDAuth(object):
         if (session.get('uid')):
             return session.get('uid')
         if audience is None:
-            audience = self._server
+            audience = request.host
         if assertion is None:
-            assertion = request.params.get('assertion')
+            assertion = \
+             urllib2.unquote(request.params.get('assertion')).strip()
         if assertion is None:
             return None
+        config = request.registry.get('config', {})
         try:
-            #note: this does not validate the assertion (yet).
-            # Still need to pull the public key from the server
-            assertion = str(urllib2.unquote(assertion)).strip()
-            parsed_cert = self._parse_assertion(assertion)
-            return parsed_cert['certificates'][0]\
-                    ['payload']['principal']['email']
+            if config.get('auth.local_auth', False):
+                #note: this does not validate the assertion (yet).
+                # Still need to pull the public key from the server
+                parsed_cert = self._parse_assertion(assertion)
+                return parsed_cert['certificates'][0]\
+                        ['payload']['principal']['email']
+            else:
+                remote_host = config.get('auth.auth_url', 
+                        'https://browserid.org/verify')
+                post_data = urllib.urlencode({'assertion': assertion,
+                    'audience': audience})
+                rb = StringIO()
+                curl = pycurl.Curl()
+                curl.setopt(pycurl.URL, remote_host)
+                curl.setopt(pycurl.FOLLOWLOCATION, 1)
+                curl.setopt(pycurl.CONNECTTIMEOUT, 30)
+                curl.setopt(pycurl.POST, 1)
+                curl.setopt(pycurl.POSTFIELDS, post_data)
+                curl.setopt(pycurl.WRITEFUNCTION, rb.write)
+                try:
+                    curl.perform()
+                except Exception, e:
+                    logger.error('Error verifying: %s' % repr(e))
+                    return None
+                curl.close()
+                response = json.loads(rb.getvalue())
+                if 'okay' in response.get('status'):
+                    return response.get('email')
+                return None
+
         except Exception, e:
             logger.info("Bad assertion [%s]" % repr(e))
             return None
@@ -44,6 +72,7 @@ class BrowserIDAuth(object):
             return urlsafe_b64decode(self._check_b64pad(str(dstr)))
         except TypeError, e:
             logger.error("Decode Error %s [%s]" % (dstr, str(e)))
+            return None
 
     def _check_b64pad(self, string):
         pad_size = 4 - (len(string) % 4)
